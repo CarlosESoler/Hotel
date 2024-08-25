901c894c989855c9a83d1bde8f44d9bc0191615c
Carlos Soler <carlinhoscaco09@gmail.com>
2024-08-25 00:59:36 -0300

    add cros config

--- src/main/java/hotel/domain/controller/HostingController.java
+++ src/main/java/hotel/domain/controller/HostingController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Transactional
 @RestController
 @RequestMapping("/hosting")
+@CrossOrigin(origins = "http://localhost:5173")
 public class HostingController {
     HostingService hostingService;
 
